9ab99c9688b11ca74675f9b370d0d82540bb2a61
Eamon Walshe <eamon.walshe@stormmq.com>
2011-10-03 10:26:30 +0100

    Rename SuperFastHash to amqp_hash to avoid collisions with application code also using the code.

--- 3rd-party/PaulHsieh/src/SuperFastHash.c
+++ 3rd-party/PaulHsieh/src/SuperFastHash.c
@@ -1,7 +1,7 @@
-#include "pstdint.h" /* Replace with <stdint.h> if appropriate */
+#include "stdint.h"
+
 #undef get16bits
-#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
-  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
+#if (defined(__GNUC__) && defined(__i386__)) || defined(_MSC_VER)
 #define get16bits(d) (*((const uint16_t *) (d)))
 #endif
 
@@ -10,7 +10,7 @@
                        +(uint32_t)(((const uint8_t *)(d))[0]) )
 #endif
 
-uint32_t SuperFastHash (const char * data, int len) {
+uint32_t amqp_hash (const char * data, int len) {
 uint32_t hash = len, tmp;
 int rem;
 
