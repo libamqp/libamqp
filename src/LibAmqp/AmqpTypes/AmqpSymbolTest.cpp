8ad9f8bb0d07e93192a2c8b9e354127c4838aa7e
Eamon Walshe <eamon.walshe@stormmq.com>
2011-10-12 09:52:27 -0400

    Test Symbol with types and static data.

--- src/LibAmqp/AmqpTypes/AmqpSymbolTest.cpp
+++ src/LibAmqp/AmqpTypes/AmqpSymbolTest.cpp
@@ -31,9 +31,14 @@ SUITE(AmqpTypesSymbol)
         static const char *c;
         static const char *d;
         static const char *e;
+
+        amqp_type_t *load();
+        void load_array();
+
     public:
         amqp_symbol_t ref;
         amqp_symbol_t *symbol_b;
+        amqp_array_t array;
     };
 
     const char *AmqpSymbolFixture::a = "ABC";
@@ -45,14 +50,28 @@ SUITE(AmqpTypesSymbol)
     AmqpSymbolFixture::AmqpSymbolFixture() :  symbol_b(0)
     {
         memset(&ref, '\0', sizeof(amqp_symbol_t));
+        memset(&array, '\0', sizeof(amqp_array_t));
     }
 
     AmqpSymbolFixture::~AmqpSymbolFixture()
     {
         amqp_symbol_cleanup(context, &ref);
         amqp_symbol_cleanup(context, symbol_b);
+            amqp_array_cleanup(context, &array);
     }
-
+        
+    amqp_type_t *AmqpSymbolFixture::load()
+    {
+        test_data::array_of_symbols.transfer_to(buffer);
+        return amqp_decode(context, buffer);
+    }
+    
+    void AmqpSymbolFixture::load_array()
+    {
+        type = load();
+        amqp_array_initialize_from_type(context, &array, type);
+    }
+    
     TEST_FIXTURE(AmqpSymbolFixture, symbol_equality)
     {
         break_one();
@@ -93,8 +112,119 @@ SUITE(AmqpTypesSymbol)
 
     TEST_FIXTURE(AmqpSymbolFixture, symbol_hash)
     {
-         amqp_symbol_initialize(context, &ref, a, strlen(a));
-         symbol_b = amqp_symbol_create(context, b, strlen(b));
+        amqp_symbol_initialize(context, &ref, a, strlen(a));
+        symbol_b = amqp_symbol_create(context, b, strlen(b));
+        CHECK_EQUAL(amqp_symbol_hash(&ref), amqp_symbol_hash(symbol_b));
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_array_decode)
+    {
+        type = load();
+        CHECK(amqp_type_is_array(type));
+        amqp_array_initialize_from_type(context, &array, type);
+
+        CHECK_EQUAL(5U, amqp_array_count(&array));
+        for (int i  = 0; i < 5; i++)
+        {
+            CHECK(amqp_type_is_symbol(amqp_array_get_type(&array, i)));
+        }
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_equality_type)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 0));
+        symbol_b = amqp_symbol_create_from_type(context, amqp_array_get_type(&array, 1));
+        CHECK(amqp_symbol_compare(&ref, symbol_b) == 0);
+        CHECK(amqp_symbol_compare(symbol_b, &ref) == 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_c_lt_d_type)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 2));
+        symbol_b = amqp_symbol_create_from_type(context, amqp_array_get_type(&array, 3));
+        CHECK(amqp_symbol_compare(&ref, symbol_b) < 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_d_gt_c_type)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 3));
+        symbol_b = amqp_symbol_create_from_type(context, amqp_array_get_type(&array, 2));
+        CHECK(amqp_symbol_compare(&ref, symbol_b) > 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_c_lt_e_type)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 2));
+        symbol_b = amqp_symbol_create_from_type(context, amqp_array_get_type(&array, 4));
+        CHECK(amqp_symbol_compare(&ref, symbol_b) < 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_e_gt_c_type)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 2));
+        symbol_b = amqp_symbol_create_from_type(context, amqp_array_get_type(&array, 4));
+        CHECK(amqp_symbol_compare(symbol_b, &ref) > 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_hash_type)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 0));
+        symbol_b = amqp_symbol_create_from_type(context, amqp_array_get_type(&array, 1));
+        CHECK_EQUAL(amqp_symbol_hash(&ref), amqp_symbol_hash(symbol_b));
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_equality_type_and_symbol)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 0));
+        symbol_b = amqp_symbol_create(context, b, strlen(b));
+        CHECK(amqp_symbol_compare(&ref, symbol_b) == 0);
+        CHECK(amqp_symbol_compare(symbol_b, &ref) == 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_c_lt_d_type_and_symbol)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 2));
+        symbol_b = amqp_symbol_create(context, d, strlen(d));
+        CHECK(amqp_symbol_compare(&ref, symbol_b) < 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_d_gt_c_type_and_symbol)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 3));
+        symbol_b = amqp_symbol_create(context, c, strlen(c));
+        CHECK(amqp_symbol_compare(&ref, symbol_b) > 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_c_lt_e_type_and_symbol)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 2));
+        symbol_b = amqp_symbol_create_from_type(context, amqp_array_get_type(&array, 4));
+        CHECK(amqp_symbol_compare(&ref, symbol_b) < 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_e_gt_c_type_and_symbol)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 2));
+        symbol_b = amqp_symbol_create(context, e, strlen(e));
+        CHECK(amqp_symbol_compare(symbol_b, &ref) > 0);
+    }
+
+    TEST_FIXTURE(AmqpSymbolFixture, symbol_hash_type_and_symbol)
+    {
+        load_array();
+        amqp_symbol_initialize_from_type(context, &ref, amqp_array_get_type(&array, 0));
+        symbol_b = amqp_symbol_create(context, b, strlen(b));
         CHECK_EQUAL(amqp_symbol_hash(&ref), amqp_symbol_hash(symbol_b));
     }
 }
