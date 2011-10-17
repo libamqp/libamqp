94248a4e736e95d32255d18705b15765d43449f9
Eamon Walshe <eamon.walshe@stormmq.com>
2011-10-17 02:39:21 +0100

    Refactor

--- src/LibAmqp/Transport/Frame/FrameEncode.c
+++ src/LibAmqp/Transport/Frame/FrameEncode.c
@@ -22,61 +22,47 @@
 
 #include "debug_helper.h"
 
-//static uint8_t sasl_frame_header[] = { 0, 0, 0, 0, 0, AMQP_SASL_FRAME_TYPE, 0, 0 };
-//static uint8_t amqp_frame_header[] = { 0, 0, 0, 0, 0, AMQP_FRAME_TYPE, 0, 0 };
-//
-//static int decode_header(amqp_context_t *context, amqp_buffer_t *buffer, amqp_frame_t *frame)
-//{
-//    assert(amqp_buffer_size(buffer) >= AMQP_FRAME_HEADER_SIZE);
-//
-//    frame->data_offset = ((uint32_t) amqp_buffer_read_uint8(buffer, 4)) * AMQP_FRAME_HEADER_DATA_OFFSET_MULTIPLIER;
-//    frame->frame_type = amqp_buffer_read_uint8(buffer, 5);
-//    frame->type_specific.word = amqp_buffer_read_uint16(buffer, 6);
-//
-//    amqp_buffer_advance_read_index(buffer, frame->data_offset);
-//
-//    if (frame->data_offset < AMQP_FRAME_HEADER_SIZE)
-//    {
-//        amqp_error(context, AMQP_ERROR_CORRUPT_FRAME_HEADER, "Invalid data offset: %u", frame->data_offset);
-//        return false;
-//    }
-//
-//    if  (frame->frame_type != AMQP_FRAME_TYPE && frame->frame_type != AMQP_SASL_FRAME_TYPE)
-//    {
-//        amqp_error(context, AMQP_ERROR_INVALID_FRAME_TYPE, "Unsupported frame type: %02x", frame->frame_type);
-//        return false;
-//    }
-//
-//    return true;
-//}
-
-
-void amqp_encode_sasl_frame(amqp_context_t *context, amqp_buffer_t *buffer)
+static inline
+void encode_frame_list(amqp_context_t *context, amqp_buffer_t *buffer)
 {
-    amqp_type_t *frame, *list;
-    size_t performativce_offset;
-    size_t frame_size;;
-    uint16_t frame_type_specific = 0;
-    
-    amqp_buffer_advance_write_point(buffer, 8);
+    amqp_type_t *list = amqp_encode_list_8(context, buffer);
+    amqp_sasl_mechanisms_encode(context, buffer);
+    amqp_complete_type(context, buffer, list);
+}
 
+static inline
+void encode_performative(amqp_context_t *context, amqp_buffer_t *buffer, uint64_t id)
+{
+    amqp_type_t *frame = amqp_start_encode_described_type(context, buffer);
+    amqp_encode_ulong(context, buffer, id);
+    encode_frame_list(context, buffer);
+    amqp_complete_type(context, buffer, frame);
+    amqp_deallocate_type(context, frame);
+}
+
+static
+void amqp_encode_frame(amqp_context_t *context, amqp_buffer_t *buffer, uint64_t id, uint8_t frame_type, uint16_t frame_type_specific)
+{
+    size_t performativce_offset, frame_size;;
+
+    amqp_buffer_advance_write_point(buffer, 8);
     performativce_offset = amqp_buffer_write_point(buffer);
 
-    frame = amqp_start_encode_described_type(context, buffer);
+    encode_performative(context, buffer, id);
 
-    amqp_encode_ulong(context, buffer, AMQP_FRAME_ID_SASL_MECHANISMS_LIST);
-    list = amqp_encode_list_8(context, buffer);
-
-amqp_sasl_mechanisms_encode(context, buffer);
-
-    amqp_complete_type(context, buffer, list);
-    amqp_complete_type(context, buffer, frame);
     frame_size = amqp_buffer_size(buffer);
     amqp_buffer_set_write_index(buffer, 0);
     amqp_buffer_write_uint32(buffer, frame_size);
     amqp_buffer_write_uint8(buffer, performativce_offset / AMQP_FRAME_HEADER_DATA_OFFSET_MULTIPLIER);
-    amqp_buffer_write_uint8(buffer, AMQP_SASL_FRAME_TYPE);
+    amqp_buffer_write_uint8(buffer, frame_type);
     amqp_buffer_write_uint16(buffer, frame_type_specific);
     amqp_buffer_set_write_index(buffer, frame_size);
-    amqp_deallocate_type(context, frame);
+}
+
+void amqp_encode_sasl_frame(amqp_context_t *context, amqp_buffer_t *buffer)
+{
+    amqp_encode_frame(context, buffer, AMQP_FRAME_ID_SASL_MECHANISMS_LIST, AMQP_SASL_FRAME_TYPE, 0);
+
+//amqp_sasl_mechanisms_encode(context, buffer);
+
 }
